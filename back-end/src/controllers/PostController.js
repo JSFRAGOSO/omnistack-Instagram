91c14bda0aff8476384f25bbb23010f8b845ce51
Jonas Fragoso Silva Fragoso <jsfragoso@ibm.com>
2019-06-11 15:09:14 -0300

    Initial Commit

---
+++ back-end/src/controllers/PostController.js
@@ -0,0 +1,22 @@
+const Post  = require('../models/Post');
+
+module.exports = {
+    async indexedDB(req,res){
+
+    },
+
+    async store (req,res){
+        const {author,place,description,hashtags} = req.body;
+        const {filename: image} = req.file;
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image,
+        })
+
+        return res.json(post)
+    },
+}
---
+++ back-end/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-m8rin.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+})
+
+app.use(require('./routes'));
+
+app.listen(3333)
